64dce5baadee1e46faaa0f828b7b5caf52524b68
rzhasibuan <rzhasibuan@gmail.com>
2021-05-31 16:34:34 +0700

    tambah comment

--- palindrome/palindrome.js
+++ palindrome/palindrome.js
@@ -1,10 +1,10 @@
-const palindrome = str => {
-    str = str.toLowerCase();
-    // jadikan str menjadi lowercase
+const palindrome = (str) => {
+  str = str.toLowerCase();
+  // jadikan str menjadi lowercase
 
-    return str === str.split('').reverse().join('');
-    // split('') berfungsi membuat string setiap hurufnya menjadi array
-    // reverse berfungsi merotasi array 
-    // join('') menggabungkan array menjadi string 
-}
-
+  return str === str.split("").reverse().join("");
+  // split('') berfungsi membuat string setiap hurufnya menjadi array
+  // reverse berfungsi merotasi array
+  // join('') menggabungkan array menjadi string
+  // tambah comment
+};
